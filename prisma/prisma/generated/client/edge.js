692c5dc5f0ce4848227de02b0ee9b17f8dfe4882
Nguyen Pham <baonguyenyam@gmail.com>
2025-04-17 17:41:31 -0500

    SizesScreen

--- prisma/prisma/generated/client/edge.js
+++ prisma/prisma/generated/client/edge.js
@@ -513,11 +513,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://postgres.uzmsljfzcboitmwlssdr:Thanhnguyen@2015@aws-0-us-west-1.pooler.supabase.com:6543/postgres?pgbouncer=true"
+        "value": null
       }
     }
   },
