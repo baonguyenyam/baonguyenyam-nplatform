6f2f2922aae07738e75531088da71f301914e105
Nguyen Pham <baonguyenyam@gmail.com>
2025-04-20 17:50:35 -0500

    Permission

--- src/app/admin/orders/permissions/view.tsx
+++ src/app/admin/orders/permissions/view.tsx
@@ -6,19 +6,25 @@ import { toast } from "sonner";
 
 import AppLoading from "@/components/AppLoading";
 import { Button } from "@/components/ui/button";
+import { Switch } from "@/components/ui/switch";
 import { enumOrderType } from "@/lib/enum";
 import { useAppDispatch, useAppSelector } from "@/store";
 import { SET_APP_STATE } from "@/store/appSlice";
 import { setAttribute } from "@/store/attributeSlice";
 
 import * as actions from "./actions";
-import { Switch } from "@/components/ui/switch";
 
 export default function View() {
 	const dispatch = useAppDispatch(); // Any where is using useAppDispatch the page will callback to CheckState
 	const [loading, setLoading] = useState(true);
-	const attrs = useAppSelector((state) => state.attributeState.data);
-	const att_orders = useMemo(() => attrs?.filter((item: any) => item.mapto === "order"), [attrs]);
+	const memoriezAttrs = useAppSelector((state) => state.attributeState.data);
+	const att_orders = useMemo(() => memoriezAttrs?.filter((item: any) => item.mapto === "order"), [memoriezAttrs]);
+	const memoriezPermission = useAppSelector((state) => (state.appState && "order_permission" in state.appState ? state.appState.order_permission : []));
+	const orderPermission = useMemo(() => {
+		return memoriezPermission as Array<{ id: number; permission: Array<{ key: string; checked: boolean }> }>;
+	}, [memoriezPermission]);
+
+	const [saveData, setSaveData] = useState<any>([]);
 
 	const fetchData = useCallback(async () => {
 		const all = await actions.getAll({ min: true, published: true });
@@ -30,7 +36,7 @@ export default function View() {
 
 	const template = (item: any) => {
 		return (
-			<>
+			<div className="w-full overflow-auto">
 				{item && item.length > 0 ? (
 					item.map((item: any) => (
 						<div
@@ -40,34 +46,29 @@ export default function View() {
 								<h3 className="text-xl font-semibold mb-2 flex items-center gap-2 border-b pb-2 mb-5">
 									<span>{item.title}</span>
 								</h3>
-								<div className="space-y-2">
+								<div className="flex flex-row items-center gap-10">
 									{item.children.map((child: any) => (
 										<div key={child.id}>
 											<div
 												key={child.id}
-												className="flex items-center gap-2">
-												<Lock className="w-4 h-4" />
+												className="flex items-center gap-2 uppercase text-xs">
 												<strong>{child.title}</strong>
 											</div>
-											<div className="ml-6 mt-2 mb-5 flex flex-wrap gap-2">
+											<div className="mt-2 mb-5 flex flex-col space-y-1">
 												{enumOrderType.map((type) => (
 													<div
 														className=""
 														key={type.value}>
-														<div className="flex items-center gap-2 text-sm">
-															{/* <input
-																type="checkbox"
-																className="form-checkbox h-4 w-4 border-gray-300 rounded"
-																id={`${child.id}-${type.value}`}
-																name={`${child.id}-${type.value}`}
-																value={type.value}
-																checked={child.attributes?.includes(type.value)}
-															/> */}
+														<div className="flex items-center gap-2 text-xs">
 															<Switch
+																className="mr-2"
 																id={`${child.id}-${type.value}`}
 																name={`${child.id}-${type.value}`}
-																defaultChecked={child.attributes?.includes(type.value)}
-																checked={child.attributes?.includes(type.value)}
+																checked={orderPermission?.find((item) => item.id === child.id)?.permission?.find((perm) => perm.key === type.value)?.checked}
+																defaultChecked={orderPermission?.find((item) => item.id === child.id)?.permission?.find((perm) => perm.key === type.value)?.checked}
+																onCheckedChange={(checked) => {
+																	///
+																}}
 															/>
 															<label htmlFor={`${child.id}-${type.value}`}>{type.label}</label>
 														</div>
@@ -83,7 +84,7 @@ export default function View() {
 				) : (
 					<p>No attributes found for user.</p>
 				)}
-			</>
+			</div>
 		);
 	};
 
@@ -124,13 +125,15 @@ export default function View() {
 												permission: enumOrderType.map((type) => {
 													return {
 														key: type.value,
-														checked: (document.getElementById(`${child.id}-${type.value}`) as HTMLInputElement)?.checked,
+														checked: orderPermission?.find((item) => item.id === child.id)?.permission?.find((perm) => perm.key === type.value)?.checked,
 													};
 												}),
 											};
 										}),
 									};
 								});
+
+								console.log("saveData", data);
 
 								const json = JSON.stringify(data);
 
