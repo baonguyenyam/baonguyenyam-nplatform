e3f8e07e99ef3f753f3161e141b06664997f9a97
Nguyen Pham <baonguyenyam@gmail.com>
2025-05-03 11:07:12 -0500

    db

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,9 +1,27 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
+// import { withAccelerate } from '@prisma/extension-accelerate'
+
+// declare global {
+// 	var prisma: PrismaClient | undefined;
+// }
+// export const db = globalThis.prisma || new PrismaClient().$extends(withAccelerate());
+
+// if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+
+
+import { PrismaClient } from '@prisma/client'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
-declare global {
-	var prisma: PrismaClient | undefined;
+const prismaClientSingleton = () => {
+	return new PrismaClient().$extends(withAccelerate())
 }
-export const db = globalThis.prisma || new PrismaClient().$extends(withAccelerate());
 
-if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+declare const globalThis: {
+	prismaGlobal: ReturnType<typeof prismaClientSingleton>
+} & typeof global
+
+const db = globalThis.prismaGlobal ?? prismaClientSingleton()
+
+export default db
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = db
