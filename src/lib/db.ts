63d5dfa23a41d0f91793fdaa3b99671cbc00fd55
Nguyen Pham <baonguyenyam@gmail.com>
2025-05-03 11:10:59 -0500

    export const db

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -20,8 +20,8 @@ declare const globalThis: {
 	prismaGlobal: ReturnType<typeof prismaClientSingleton>
 } & typeof global
 
-const db = globalThis.prismaGlobal ?? prismaClientSingleton()
+// const db = globalThis.prismaGlobal ?? prismaClientSingleton()
 
-export default db
+export const db = globalThis.prismaGlobal || prismaClientSingleton()
 
 if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = db
