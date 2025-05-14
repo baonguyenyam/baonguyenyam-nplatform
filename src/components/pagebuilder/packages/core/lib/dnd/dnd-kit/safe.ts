32e6e1165251ba33341e986480a14a8e2e40cb7c
Nguyen Pham <baonguyenyam@gmail.com>
2025-05-14 09:24:30 -0500

    Fixed

--- src/components/pagebuilder/packages/core/lib/dnd/dnd-kit/safe.ts
+++ src/components/pagebuilder/packages/core/lib/dnd/dnd-kit/safe.ts
@@ -4,7 +4,7 @@ import { useSortable, UseSortableInput } from "@dnd-kit/react/sortable";
 
 export function useDroppableSafe<T extends Data>(input: UseDroppableInput<T>): Pick<ReturnType<typeof useDroppable<T>>, "ref"> {
 	if (typeof window === "undefined") {
-		return { ref: () => {} };
+		return { ref: () => { } };
 	}
 
 	// eslint-disable-next-line react-hooks/rules-of-hooks
@@ -13,16 +13,16 @@ export function useDroppableSafe<T extends Data>(input: UseDroppableInput<T>): P
 
 export function useDraggableSafe<T extends Data>(input: UseDraggableInput): Pick<ReturnType<typeof useDraggable<T>>, "ref"> {
 	if (typeof window === "undefined") {
-		return { ref: () => {} };
+		return { ref: () => { } };
 	}
 
 	// eslint-disable-next-line react-hooks/rules-of-hooks
 	return useDraggable(input);
 }
 
-export function useSortableSafe<T extends Data>(input: UseSortableInput<T>): Pick<ReturnType<typeof useSortable<T>>, "ref" | "status" | "handleRef"> {
+export function useSortableSafe<T extends Data>(input: UseSortableInput<T>): Pick<ReturnType<typeof useSortable<T>>, any> {
 	if (typeof window === "undefined") {
-		return { ref: () => {}, status: "idle", handleRef: () => {} };
+		return { ref: () => { }, status: "idle", handleRef: () => { } };
 	}
 
 	// eslint-disable-next-line react-hooks/rules-of-hooks
