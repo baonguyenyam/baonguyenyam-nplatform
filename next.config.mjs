d068d93d8972d6f54ca7f5b3fc4c8d62713c36b5
Nguyen Pham <admin@madelab.dev>
2025-05-20 15:30:28 -0500

    Update

--- next.config.mjs
+++ next.config.mjs
@@ -1,61 +1,62 @@
 const nextConfig = {
-  env: {
-    public_APIUrl: process.env.PUBLIC_API_URL,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  compress: true,
-  crossOrigin: 'anonymous',
-  poweredByHeader: false,
-  reactStrictMode: true,
-  sassOptions: {
-    silenceDeprecations: ['legacy-js-api'],
-  },
-  experimental: {
-    serverActions: {
-      // Limit file upload size
-      bodySizeLimit: process.env.MAX_FILE_SIZE
-    },
-    webpackMemoryOptimizations: true,
-    cssChunking: true,
-	 	nextScriptWorkers: false,
+	env: {
+		public_APIUrl: process.env.PUBLIC_API_URL,
+	},
+	eslint: {
+		ignoreDuringBuilds: true,
+		dirs: ["src/components/editor", "src/components/pagebuilder"],
+	},
+	compress: true,
+	crossOrigin: "anonymous",
+	poweredByHeader: false,
+	reactStrictMode: true,
+	sassOptions: {
+		silenceDeprecations: ["legacy-js-api"],
+	},
+	experimental: {
+		serverActions: {
+			// Limit file upload size
+			bodySizeLimit: process.env.MAX_FILE_SIZE,
+		},
+		webpackMemoryOptimizations: true,
+		cssChunking: true,
+		nextScriptWorkers: false,
 		inlineCss: true,
 		reactCompiler: {
-			compilationMode: 'annotation',
+			compilationMode: "annotation",
 		},
 		staleTimes: {
 			dynamic: 30,
 			static: 180,
 		},
-  },
-   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "*", 
-      },
-      {
-        protocol: "http",
-        hostname: "*", 
-      },
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'avatars.githubusercontent.com',
-      },
-    ],
-  }
+	},
+	typescript: {
+		// !! WARN !!
+		// Dangerously allow production builds to successfully complete even if
+		// your project has type errors.
+		// !! WARN !!
+		ignoreBuildErrors: true,
+	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "*",
+			},
+			{
+				protocol: "http",
+				hostname: "*",
+			},
+			{
+				protocol: "https",
+				hostname: "lh3.googleusercontent.com",
+			},
+			{
+				protocol: "https",
+				hostname: "avatars.githubusercontent.com",
+			},
+		],
+	},
 };
 
 export default nextConfig;
