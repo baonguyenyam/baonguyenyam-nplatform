090c6aab169e82e0c388be32118e9533c160c369
Nguyen Pham <admin@madelab.dev>
2025-05-20 15:04:44 -0500

    ignoreBuildErrors

--- next.config.mjs
+++ next.config.mjs
@@ -19,6 +19,13 @@ const nextConfig = {
     },
       webpackMemoryOptimizations: true,
   },
+   typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
   images: {
     remotePatterns: [
       {
